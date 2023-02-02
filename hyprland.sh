0c46f673cbf02cea0a0302906f4eb2e668797732
ypli0629 <liyapeng0629@gmail.com>
2023-02-02 20:43:24 +0800

    feat: add hyprland

---
+++ hyprland.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+yay -S hyprland-git --noconfirm
+yay -S rofi dunst kitty swaybg swaylock-swaylock-fancy swayidle pamixer light brillo --noconfirm
+yay -S bc blueberry bluez coreutils dbus findutils gawk gnunet jaq light networkmanager network-manager-applet pavucontrol playerctl procps ripgrep socat udev upower util-linux wget wireplumber wlogout eww-wayland --noconfirm
+yay -S grim swappy slurp --noconfirm
+yay -S qt5-wayland qt6-wayland --noconfirm
+
+cp -r kitty hypr rofi dunst swaylock eww ~/.config
