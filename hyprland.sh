4e1840b6bd76d0d0047e7c754748a9797ea99599
ypli0629 <liyapeng0629@gmail.com>
2023-02-02 21:34:16 +0800

    feat: add swaylock

--- hyprland.sh
+++ hyprland.sh
@@ -2,7 +2,7 @@
 
 yay -S hyprland-git --noconfirm
 # yay -S hyprland-nvidia-git --noconfirm
-yay -S rofi dunst kitty swaybg swaylock-swaylock-fancy swayidle pamixer light brillo --noconfirm
+yay -S rofi dunst kitty swaybg swaylock-fancy-git swayidle pamixer light brillo --noconfirm
 yay -S bc blueberry bluez coreutils dbus findutils gawk gnunet jaq light networkmanager network-manager-applet pavucontrol playerctl procps ripgrep socat udev upower util-linux wget wireplumber wlogout eww-wayland --noconfirm
 yay -S grim swappy slurp --noconfirm
 yay -S qt5-wayland qt6-wayland --noconfirm
