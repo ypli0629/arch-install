89a3f4b29505b401ae3c6283c81e36ff439efd3d
ypli0629 <liyapeng0629@gmail.com>
2023-02-02 21:26:59 +0800

    feat: add hyprland nvidia

--- hyprland.sh
+++ hyprland.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 yay -S hyprland-git --noconfirm
+# yay -S hyprland-nvidia-git --noconfirm
 yay -S rofi dunst kitty swaybg swaylock-swaylock-fancy swayidle pamixer light brillo --noconfirm
 yay -S bc blueberry bluez coreutils dbus findutils gawk gnunet jaq light networkmanager network-manager-applet pavucontrol playerctl procps ripgrep socat udev upower util-linux wget wireplumber wlogout eww-wayland --noconfirm
 yay -S grim swappy slurp --noconfirm
