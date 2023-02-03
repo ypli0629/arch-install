fa81805be06d3b4cd2939cb614ea2cf0e49a7f5d
ypli0629 <liyapeng0629@gmail.com>
2023-02-03 10:57:48 +0800

    feat: add exec permission

--- hyprland.sh
+++ hyprland.sh
@@ -2,8 +2,8 @@
 
 yay -S hyprland-git --noconfirm
 # yay -S hyprland-nvidia-git --noconfirm
-yay -S rofi dunst kitty swaybg swaylock-fancy-git swayidle pamixer light brillo --noconfirm
-yay -S bc blueberry bluez coreutils dbus findutils gawk gnunet jaq light networkmanager network-manager-applet pavucontrol playerctl procps ripgrep socat udev upower util-linux wget wireplumber wlogout eww-wayland --noconfirm
+yay -S rofi dunst kitty swaybg swaylock-fancy-git swayidle pamixer wlogout eww-wayland light brillo --noconfirm
+yay -S bc blueberry bluez coreutils dbus findutils gawk gnunet jaq networkmanager network-manager-applet pavucontrol playerctl procps ripgrep socat udev upower util-linux wget wireplumber --noconfirm
 yay -S grim swappy slurp --noconfirm
 yay -S qt5-wayland qt6-wayland --noconfirm
 
