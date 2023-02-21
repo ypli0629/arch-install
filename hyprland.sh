bbe246353cdd9b21aefbcec7a89fa7c550abc31a
ypli0629 <liyapeng0629@gmail.com>
2023-02-21 22:33:03 +0800

    feat: update

--- hyprland.sh
+++ hyprland.sh
@@ -3,8 +3,8 @@
 yay -S hyprland-git --noconfirm
 # yay -S hyprland-nvidia-git --noconfirm
 yay -S rofi dunst kitty swaybg swaylock-fancy-git swayidle pamixer wlogout eww-wayland light brillo --noconfirm
-yay -S bc blueberry bluez coreutils dbus findutils gawk gnunet jaq networkmanager network-manager-applet pavucontrol playerctl procps ripgrep socat udev upower util-linux wget wireplumber --noconfirm
-yay -S grim swappy slurp --noconfirm
+yay -S bc blueberry bluez coreutils dbus findutils gawk gnunet jaq light networkmanager network-manager-applet pavucontrol playerctl procps ripgrep socat udev upower util-linux wget wireplumber wlogout eww-wayland pipewire-pulse --noconfirm
+yay -S grim swappy slurp rofi-emoji --noconfirm
 yay -S qt5-wayland qt6-wayland --noconfirm
 
 cp -rf ./config/* ~/.config
