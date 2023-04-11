de791ed10644877bb15f94acd22c3cb05278af27
ypli0629 <liyapeng0629@gmail.com>
2023-04-11 09:59:47 +0800

    feat: add gnome keyboard settings

---
+++ gnome.sh
@@ -0,0 +1,10 @@
+#!.bin.bash
+for i in $(seq 1 9)
+do 
+    gsettings set org.gnome.shell.keybindings switch-to-application-${i} '[]';
+    gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-${i} "['<Shift><Super>"${i}"']";
+    gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-${i} "['<Super>"${i}"']";
+done;
+
+gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "['<Super>f']"
+gsettings set org.gnome.desktop.wm.keybindings close "['<Super>q']"
