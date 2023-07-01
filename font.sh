e8da939a07f8a275afba001f67a1bd511ae01eb9
ypli <liyapeng0629@gmail.com>
2023-07-01 20:57:26 +0800

    feat: add font

--- font.sh
+++ font.sh
@@ -4,4 +4,4 @@
 yay -S ttf-sourcecodepro-nerd ttf-nerd-fonts-symbols noto-fonts-emoji ttf-ms-fonts wqy-microhei --noconfirm
 
 # fcitx5
-yay -S fcitx5-im fcitx5-input-support fcitx5-chinese-addons --noconfirm
+yay -S fcitx5-im fcitx5-input-support fcitx5-chinese-addons fcitx5-pinyin-custom-pinyin-dictionary fcitx5-pinyin-zhwiki --noconfirm
