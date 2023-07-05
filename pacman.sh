0d30b7ef118d8f7ff382485af88705cc13446c1e
ypli0629 <liyapeng0629@gmail.com>
2023-07-05 22:57:06 +0800

    feat: optimize

--- pacman.sh
+++ pacman.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # mirror
-sudo tee /etc/pacman.d/mirrorlist <<EOF
-Server = https://mirror.sjtu.edu.cn/archlinux/\$repo/os/\$arch
-EOF
+# sudo tee /etc/pacman.d/mirrorlist <<EOF
+# Server = https://mirror.sjtu.edu.cn/archlinux/\$repo/os/\$arch
+# EOF
+sudo pacman-mirrors --api --set-branch testing
+sudo pacman-mirrors --fasttrack 5
 sudo tee -a /etc/pacman.conf <<EOF
 [archlinuxcn]
 Server = https://mirror.sjtu.edu.cn/archlinux-cn/\$arch
@@ -12,3 +14,4 @@ EOF
 # archlinuxcn
 sudo pacman -Syy
 sudo pacman -S archlinuxcn-keyring --noconfirm
+sudo pacman -Syyu
