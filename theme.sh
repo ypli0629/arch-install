8694324d933cfd1ba23b2f7c24fdb0f3c5de8aa3
ypli0629 <liyapeng0629@gmail.com>
2023-12-28 23:47:30 +0800

    feat: add themes

--- theme.sh
+++ theme.sh
@@ -3,29 +3,8 @@ mkdir themes
 
 cd themes
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
-cd WhiteSur-gtk-theme
-sudo ./install.sh --silent-mode
-sudo ./tweaks.sh -g x2 --silent-mode
-cd ..
-
 git clone https://github.com/vinceliuice/grub2-themes.git
-cd grub2-themes
-sudo ./install.sh -b -t whitesur -i whitesur -s 2k
-cd ..
-
 git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
-cd WhiteSur-icon-theme
-./install.sh -b
-cd ..
-
 git clone https://github.com/vinceliuice/WhiteSur-wallpapers.git
-cd WhiteSur-wallpapers
-sudo ./install-gnome-backgrounds.sh -s 2k
-cd ..
-
 git clone https://github.com/vinceliuice/WhiteSur-cursors.git
-cd WhiteSur-cursors
-sudo ./install.sh
-cd ..
 
-cd ..
