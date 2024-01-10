d599a13e9f3cfa61e5b7e70ba6acadedf09547d2
ypli0629 <liyapeng0629@gmail.com>
2024-01-10 23:38:43 +0800

    feat: use nju mirror

--- pacman.sh
+++ pacman.sh
@@ -8,7 +8,12 @@ sudo pacman-mirrors --api --set-branch testing
 sudo pacman-mirrors --fasttrack 5
 sudo tee -a /etc/pacman.conf <<EOF
 [archlinuxcn]
+SigLevel = TrustAll
 Server = https://mirror.sjtu.edu.cn/archlinux-cn/\$arch
+EOF
+sudo tee /etc/pacman.d/mirrorlist <<EOF
+[archlinuxcn]
+Server = https://mirror.nju.edu.cn/archlinux/\$repo/os/\$arch
 EOF
 
 # archlinuxcn
