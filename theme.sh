bd7f343e975d52bef9200a2c445a866001b0f720
ypli0629 <liyapeng0629@gmail.com>
2024-04-14 19:13:58 +0800

    feat: add plymouth theme

--- theme.sh
+++ theme.sh
@@ -4,13 +4,13 @@ mkdir themes
 cd themes
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
 cd WhiteSur-gtk-theme
-./install.sh
+./install.sh -i arch -l -N stable
 cd ..
 
-git clone https://github.com/vinceliuice/grub2-themes.git
-cd grub2-themes
-sudo ./install.sh -t vimix -s 2k -b
-cd ..
+#git clone https://github.com/vinceliuice/grub2-themes.git
+#cd grub2-themes
+#sudo ./install.sh -t vimix -s 2k -b
+#cd ..
 
 git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
 cd WhiteSur-icon-theme
